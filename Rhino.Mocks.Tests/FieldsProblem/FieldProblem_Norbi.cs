b221aac26cc9b378b0f4dd34934e7bb8ff00f80e
Mike Meisinger <mike.meisinger@gmail.com>
2014-04-02 23:01:47 -0400

    added two field problems
    validation that latest version does not throw an exception when setting
    two expectations against a method that returns a func

---
+++ Rhino.Mocks.Tests/FieldsProblem/FieldProblem_Norbi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xunit;
+
+namespace Rhino.Mocks.Tests.FieldsProblem
+{
+    public class FieldProblem_Norbi
+    {
+        public interface IFuncTest<T>
+        {
+            Func<T> Get(string text);
+        }
+
+        public interface IParam
+        {
+            string Text { get; }
+        }
+
+        public class Param : IParam
+        {
+            public string Text { get; set; }
+        }
+
+        [Fact]
+        public void Can_Create_Two_Expectations_Against_Func()
+        {
+            var mock = MockRepository.Mock<IFuncTest<IParam>>();
+            mock.Expect(x => x.Get("Test1"))
+                .Return(() => new Param { Text = "ParamWithText1" });
+
+            mock.Expect(x => x.Get("Test2"))
+                .Return(() => new Param { Text = "ParamWithText2" });
+
+            Assert.Equal("ParamWithText1", mock.Get("Test1")().Text);
+            Assert.Equal("ParamWithText2", mock.Get("Test2")().Text);
+
+            mock.VerifyExpectations();
+        }
+    }
+}
---
+++ Rhino.Mocks.Tests/FieldsProblem/FieldProblem_SO_user815512.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xunit;
+
+namespace Rhino.Mocks.Tests.FieldsProblem
+{
+    public class FieldProblem_SO_user815512
+    {
+        public interface IFuncTest
+        {
+            Func<int> Generate(int num);
+        }
+
+        [Fact]
+        public void Can_Create_Two_Expectations_With_No_Exception()
+        {
+            var myMock = MockRepository.Mock<IFuncTest>();
+
+            myMock.Expect(x => x.Generate(42))
+                .Return(() => 6);
+
+            myMock.Expect(x => x.Generate(4000))
+                .Return(() => 9);
+
+            Assert.Equal(6, myMock.Generate(42)());
+            Assert.Equal(9, myMock.Generate(4000)());
+
+            myMock.VerifyExpectations(true);
+        }
+    }
+}
